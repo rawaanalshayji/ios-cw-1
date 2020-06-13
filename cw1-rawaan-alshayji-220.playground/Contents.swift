09b3cd1e93c98cb69e5b8bebe40c1957cb441505
rawaanalshayji <66734178+rawaanalshayji@users.noreply.github.com>
2020-06-13 20:45:08 +0300

    done

---
+++ cw1-rawaan-alshayji-220.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let name = " rawaan "
+var age = 16
+var height = 157
+var hobby1 = " drawing "
+var hobby2 = " singing "
+
+
+
+print ( " my name is: \(name) my age is \(age) and my hight is \(height) my hobby is \(hobby1) and \(hobby2) " )
+
+let friendName = "jenaan"
+ var friendAge = 16
+var friendHight = 160
+var friendHobby1 = "drawing"
+var friendHobby2 = "cooking"
+ var age_difference = age - friendAge
+
+age > friendAge
+ height <= friendHight
+hobby2 == friendHobby1
